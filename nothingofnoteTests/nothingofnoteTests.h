0a22d350c156cf84877b3030308b5ae082ee4e16
Shelby Munsch <smunsch@gmail.com>
2012-05-14 13:32:29 -0700

    Initial Commit

---
+++ nothingofnoteTests/nothingofnoteTests.h
@@ -0,0 +1,13 @@
+//
+//  nothingofnoteTests.h
+//  nothingofnoteTests
+//
+//  Created by Shelby Munsch on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface nothingofnoteTests : SenTestCase
+
+@end
